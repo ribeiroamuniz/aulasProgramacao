3479e3317e6bf9243c693edb47c923384c45c846
angelica <angelica-rmuniz@educar.rs.gov.br>
2024-10-04 06:33:30 -0300

    hospital0410

---
+++ src/model/Adicionar.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Adicionar {
+	private ArrayList<Paciente> pacientes;
+	
+	
+	public Adicionar(){
+		pacientes = new ArrayList<>(); 
+	}
+	
+}
